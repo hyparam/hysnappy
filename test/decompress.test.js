343a9d5ed612ffb25ad65ed10a260aa1eb82245b
Kenny Daniel <platypii@gmail.com>
2024-02-17 22:44:33 -0800

    eslint

--- test/decompress.test.js
+++ test/decompress.test.js
@@ -1,10 +1,10 @@
-import { describe, it, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { snappyUncompress } from '../snappy.js'
 
 describe('snappy uncompress', () => {
   it('decompresses a compressed string correctly', async () => {
     const compressed = new Uint8Array([
-      0x0a, 0x24, 0x68, 0x79, 0x70, 0x65, 0x72, 0x70, 0x61, 0x72, 0x61, 0x6d
+      0x0a, 0x24, 0x68, 0x79, 0x70, 0x65, 0x72, 0x70, 0x61, 0x72, 0x61, 0x6d,
     ])
     const output = new Uint8Array(10)
     const expected = 'hyperparam'
