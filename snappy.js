455db7d664044071042cbe2269dcd4b63fb8aaee
Kenny Daniel <platypii@gmail.com>
2024-02-18 14:39:02 -0800

    Fix typescript errors and add to CI

--- snappy.js
+++ snappy.js
@@ -12,24 +12,33 @@ export async function snappyUncompress(input, output) {
 
   // Load the WASM module
   const snappyModule = await WebAssembly.instantiate(new Uint8Array(wasmBuffer), {})
-  const { memory, uncompress } = snappyModule.instance.exports
+  // const { memory, uncompress } = snappyModule.instance.exports
+  /** @type {WebAssembly.Memory} */
+  // @ts-ignore
+  // eslint-disable-next-line prefer-destructuring
+  const memory = snappyModule.instance.exports.memory
+  /** @type {Function} */
+  // @ts-ignore
+  // eslint-disable-next-line prefer-destructuring
+  const uncompress = snappyModule.instance.exports.uncompress
 
   // WASM memory
+  /** @type {WebAssembly.Memory} */
   if (memory.buffer.byteLength < input.byteLength + output.byteLength) {
     // TODO: memory.grow(pagesToGrow)
     throw new Error('memory buffer is too small')
   }
 
   // Copy the compressed data to WASM memory
-  const buffer = new Uint8Array(memory.buffer)
-  buffer.set(input)
+  const byteArray = new Uint8Array(memory.buffer)
+  byteArray.set(input)
 
   // Call wasm uncompress function
   const result = uncompress(0, input.byteLength, input.byteLength)
   if (result !== 0) throw new Error(`failed to uncompress data ${result}`)
 
   // Get uncompressed data from WASM memory
-  const uncompressed = buffer.slice(input.byteLength, input.byteLength + output.byteLength)
+  const uncompressed = byteArray.slice(input.byteLength, input.byteLength + output.byteLength)
 
   // Copy the uncompressed data to the output buffer
   // TODO: Return WASM memory buffer instead of copying?
